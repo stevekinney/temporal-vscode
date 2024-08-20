0fea876a0299affe31f820bf2ba948a201618234
Steve Kinney <hello@stevekinney.net>
2024-08-19 18:08:48 -0600

    Update configuration.ts

--- src/utilities/configuration.ts
+++ src/utilities/configuration.ts
@@ -1,10 +1,13 @@
 import * as vscode from 'vscode';
 
+const getConfiguration = <T = string | undefined>(
+  key: string,
+  section: string = 'temporal',
+) => vscode.workspace.getConfiguration(section).get<T>(key);
+
 export const configuration = {
   get address() {
-    return vscode.workspace
-      .getConfiguration('temporal')
-      .get('address.server') as string;
+    return getConfiguration('address.server') || 'localhost:7233';
   },
   get host() {
     return this.address.split(':')[0];
@@ -13,31 +16,22 @@ export const configuration = {
     return parseInt(this.address.split(':')[1]);
   },
   get ui() {
-    const address = vscode.workspace
-      .getConfiguration('temporal')
-      .get('address.webUI') as string;
+    const address =
+      getConfiguration<string>('address.webUI') || 'http://localhost:8233';
 
     return new URL(address);
   },
   get namespace() {
-    return vscode.workspace
-      .getConfiguration('temporal')
-      .get('namespace') as string;
+    return getConfiguration<string>('namespace') || 'default';
   },
   get identity() {
-    return vscode.workspace.getConfiguration('temporal').get('identity') as
-      | string
-      | undefined;
+    return getConfiguration<string>('identity');
   },
   get apiKey() {
-    return vscode.workspace.getConfiguration('temporal').get('apiKey') as
-      | string
-      | undefined;
+    return getConfiguration<string | undefined>('apiKey');
   },
   get codecEndpoint() {
-    const address = vscode.workspace
-      .getConfiguration('temporal')
-      .get('address.codecEndpoint') as string;
+    const address = getConfiguration('address<string>.codecEndpoint');
 
     if (!address) {
       return;
@@ -46,13 +40,7 @@ export const configuration = {
     return new URL(address);
   },
   get logLevel() {
-    const logLevel = vscode.workspace
-      .getConfiguration('temporal')
-      .get('logLevel') as string;
-
-    if (!logLevel) {
-      return 'error';
-    }
+    const logLevel = getConfiguration<string>('logLevel') || 'error';
 
     return logLevel;
   },
