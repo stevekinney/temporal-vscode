3b01673b2b94693d42b99267a07c0ac229ae3350
Steve Kinney <hello@stevekinney.net>
2025-04-28 11:22:33 -0600

    Update select.ts

--- src/utilities/select.ts
+++ src/utilities/select.ts
@@ -2,29 +2,31 @@ import * as vscode from 'vscode';
 import pluralize from 'pluralize';
 import type { TemporalClient } from './client';
 
+export interface SelectOptions<T, F extends string> extends vscode.QuickPickOptions {
+  client: TemporalClient;
+  name: string;
+  data: (client: TemporalClient) => Promise<ReadonlyArray<T> | ArrayLike<T>>;
+  format: (item: T) => F;
+}
+
 export const select = async <T, F extends string>({
   client,
   name,
   data,
   format,
   ...options
-}: {
-  client: TemporalClient;
-  name: string;
-  data: (client: TemporalClient) => Promise<ArrayLike<T>>;
-  format: (item: T) => F;
-} & vscode.QuickPickOptions): Promise<T> => {
+}: SelectOptions<T, F>): Promise<T> => {
   const items = await data(client);
 
   if (!Array.isArray(items) || items.length === 0) {
     throw new Error(`No ${pluralize(name)} found.`);
   }
 
-  const listItems = items.map(format);
+  const listItems = Array.from(items).map(format);
   const selectedId = await vscode.window.showQuickPick(listItems, options);
 
   if (!selectedId) {
-    new Error(`User Aborted: No ${name} selected.`);
+    throw new Error(`User Aborted: No ${name} selected.`);
   }
 
   const selectedItem = items.find((item) => format(item) === selectedId);
