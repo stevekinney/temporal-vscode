08aad9a1ff346feb3ac2f40208f6714494c1ec49
Steve Kinney <hello@stevekinney.net>
2025-04-28 10:38:39 -0600

    Update status-bar-item.ts

--- src/components/status-bar-item.ts
+++ src/components/status-bar-item.ts
@@ -1,178 +1,448 @@
 import * as vscode from 'vscode';
 import { toCommandName } from '$components/command';
 
-export type StatusBarItemParameters = {
+/**
+ * Parameters for creating status bar items
+ */
+export interface StatusBarItemParameters {
   id: string;
   alignment?: vscode.StatusBarAlignment;
   priority?: number;
   tooltip?: string;
   text?: string;
-  command?: CommandName;
-  color?: string;
-};
+  command?: CommandName | (() => void | Promise<void>);
+  color?: string | vscode.ThemeColor;
+  backgroundColor?: vscode.ThemeColor;
+  name?: string;
+}
 
+/**
+ * A wrapper around vscode.StatusBarItem with additional functionality
+ */
 export class StatusBarItem implements vscode.Disposable {
   static context: vscode.ExtensionContext;
   static readonly items: StatusBarItem[] = [];
 
+  /**
+   * Creates a new status bar item with the given parameters
+   * @param parameters The parameters to create the status bar item with
+   * @returns A new StatusBarItem instance
+   */
   static create(parameters: StatusBarItemParameters): StatusBarItem {
+    const existing = this.get(parameters.id);
+    if (existing) {
+      existing.update(parameters);
+      return existing;
+    }
     return new StatusBarItem(parameters);
   }
 
+  /**
+   * Gets a status bar item by ID
+   * @param id The ID of the status bar item to find
+   * @returns The status bar item or undefined if not found
+   */
   static get(id: string): StatusBarItem | undefined {
     return this.items.find((item) => item.id === id);
   }
 
+  /**
+   * Disposes a status bar item by ID
+   * @param id The ID of the status bar item to dispose
+   */
   static dispose(id: string): void {
-    const item = this.get(id);
-
-    if (item) {
-      item.dispose();
+    const index = this.items.findIndex((item) => item.id === id);
+    if (index !== -1) {
+      this.items[index].dispose();
+      this.items.splice(index, 1);
     }
   }
 
+  /**
+   * Disposes all status bar items
+   */
   static disposeAll(): void {
     this.items.forEach((item) => item.dispose());
     this.items.length = 0;
   }
 
-  private readonly item;
-  private interval: NodeJS.Timer | undefined = undefined;
+  private readonly _item: vscode.StatusBarItem;
+  private _interval: NodeJS.Timer | undefined = undefined;
+  private _disposed: boolean = false;
+  private _commandHandler: vscode.Disposable | undefined = undefined;
+  private _commandCallback: (() => void | Promise<void>) | undefined = undefined;
 
-  constructor({
-    id,
-    alignment,
-    priority,
-    tooltip,
-    text,
-    color,
-    command,
-  }: StatusBarItemParameters) {
-    this.item = vscode.window.createStatusBarItem(id, alignment, priority);
-
-    if (tooltip) {
-      this.item.tooltip = tooltip;
-    }
-
-    if (text) {
-      this.item.text = text;
-    }
-
-    if (color) {
-      this.item.color = color;
-    }
-
-    if (command) {
-      this.item.command = toCommandName(command);
-    }
+  constructor(parameters: StatusBarItemParameters) {
+    const { id, alignment = vscode.StatusBarAlignment.Left, priority = 0, name } = parameters;
+    
+    this._item = vscode.window.createStatusBarItem(id, alignment, priority, name);
+    this.update(parameters);
 
     StatusBarItem.items.push(this);
-    StatusBarItem.context.subscriptions.push(this.item);
+    StatusBarItem.context.subscriptions.push(this);
   }
 
-  get id(): string {
-    return this.item.id;
-  }
+  /**
+   * Updates the status bar item with new parameters
+   * @param parameters The parameters to update
+   * @returns This status bar item for chaining
+   */
+  update(parameters: Partial<StatusBarItemParameters>): StatusBarItem {
+    try {
+      const { tooltip, text, color, backgroundColor, command } = parameters;
 
-  get text(): string {
-    return this.item.text;
-  }
+      if (tooltip !== undefined) {
+        this._item.tooltip = tooltip;
+      }
 
-  set text(value: string) {
-    this.item.text = value;
-  }
+      if (text !== undefined) {
+        this._item.text = text;
+      }
 
-  get tooltip() {
-    return this.item.tooltip;
-  }
+      if (color !== undefined) {
+        this._item.color = color;
+      }
 
-  set tooltip(value: typeof this.item.tooltip) {
-    this.item.tooltip = value;
-  }
+      if (backgroundColor !== undefined) {
+        this._item.backgroundColor = backgroundColor;
+      }
 
-  get color() {
-    return this.item.color;
-  }
+      if (command !== undefined) {
+        this.setCommand(command);
+      }
 
-  set color(value: typeof this.item.color) {
-    this.item.color = value;
-  }
-
-  show(): void {
-    this.item.show();
-  }
-
-  hide(): void {
-    this.item.hide();
-  }
-
-  dispose(): void {
-    this.item.dispose();
-    this.clearInterval();
-  }
-
-  setInterval(callback: () => void, interval: number): void {
-    this.clearInterval();
-    this.interval = setInterval(callback, interval);
-  }
-
-  clearInterval(): void {
-    if (this.interval) {
-      clearInterval(this.interval);
-      this.interval = undefined;
+      return this;
+    } catch (error) {
+      console.error(`Failed to update status bar item ${this.id}:`, error);
+      return this;
     }
+  }
+
+  /**
+   * Sets the command for this status bar item
+   * @param command The command to execute when the status bar item is clicked
+   * @returns This status bar item for chaining
+   */
+  private setCommand(command: CommandName | (() => void | Promise<void>)): StatusBarItem {
+    // Clean up any existing command handler
+    if (this._commandHandler) {
+      this._commandHandler.dispose();
+      this._commandHandler = undefined;
+      this._commandCallback = undefined;
+    }
+
+    if (typeof command === 'string') {
+      // Set the standard command name
+      this._item.command = toCommandName(command);
+    } else if (typeof command === 'function') {
+      // Create a unique command ID for this callback
+      const commandId = `statusBarItem.${this.id}.command`;
+      this._commandCallback = command;
+      
+      // Register the command
+      this._commandHandler = vscode.commands.registerCommand(commandId, async () => {
+        try {
+          if (this._commandCallback) {
+            await this._commandCallback();
+          }
+        } catch (error) {
+          console.error(`Error executing status bar command for ${this.id}:`, error);
+        }
+      });
+      
+      // Add to extension subscriptions
+      StatusBarItem.context.subscriptions.push(this._commandHandler);
+      
+      // Set the command
+      this._item.command = commandId;
+    }
+
+    return this;
+  }
+
+  /**
+   * Gets the ID of this status bar item
+   */
+  get id(): string {
+    return this._item.id;
+  }
+
+  /**
+   * Gets the text of this status bar item
+   */
+  get text(): string {
+    return this._item.text;
+  }
+
+  /**
+   * Sets the text of this status bar item
+   */
+  set text(value: string) {
+    if (!this._disposed) {
+      this._item.text = value;
+    }
+  }
+
+  /**
+   * Gets the tooltip of this status bar item
+   */
+  get tooltip() {
+    return this._item.tooltip;
+  }
+
+  /**
+   * Sets the tooltip of this status bar item
+   */
+  set tooltip(value: typeof this._item.tooltip) {
+    if (!this._disposed) {
+      this._item.tooltip = value;
+    }
+  }
+
+  /**
+   * Gets the color of this status bar item
+   */
+  get color() {
+    return this._item.color;
+  }
+
+  /**
+   * Sets the color of this status bar item
+   */
+  set color(value: typeof this._item.color) {
+    if (!this._disposed) {
+      this._item.color = value;
+    }
+  }
+
+  /**
+   * Gets the background color of this status bar item
+   */
+  get backgroundColor() {
+    return this._item.backgroundColor;
+  }
+
+  /**
+   * Sets the background color of this status bar item
+   */
+  set backgroundColor(value: vscode.ThemeColor | undefined) {
+    if (!this._disposed) {
+      this._item.backgroundColor = value;
+    }
+  }
+
+  /**
+   * Shows the status bar item
+   * @returns This status bar item for chaining
+   */
+  show(): StatusBarItem {
+    if (!this._disposed) {
+      this._item.show();
+    }
+    return this;
+  }
+
+  /**
+   * Hides the status bar item
+   * @returns This status bar item for chaining
+   */
+  hide(): StatusBarItem {
+    if (!this._disposed) {
+      this._item.hide();
+    }
+    return this;
+  }
+
+  /**
+   * Disposes the status bar item and cleans up resources
+   */
+  dispose(): void {
+    if (!this._disposed) {
+      this.clearInterval();
+      
+      if (this._commandHandler) {
+        this._commandHandler.dispose();
+        this._commandHandler = undefined;
+      }
+      
+      this._item.dispose();
+      this._disposed = true;
+      
+      // Remove from items array
+      const index = StatusBarItem.items.indexOf(this);
+      if (index !== -1) {
+        StatusBarItem.items.splice(index, 1);
+      }
+    }
+  }
+
+  /**
+   * Sets an interval for updating this status bar item
+   * @param callback The function to call on the interval
+   * @param interval The interval in milliseconds
+   * @returns This status bar item for chaining
+   */
+  setInterval(callback: () => void, interval: number): StatusBarItem {
+    this.clearInterval();
+    if (!this._disposed) {
+      this._interval = setInterval(callback, interval);
+    }
+    return this;
+  }
+
+  /**
+   * Clears the update interval for this status bar item
+   * @returns This status bar item for chaining
+   */
+  clearInterval(): StatusBarItem {
+    if (this._interval) {
+      clearInterval(this._interval);
+      this._interval = undefined;
+    }
+    return this;
   }
 }
 
-export class StatusBarItemGroup {
-  items: StatusBarItem[];
+/**
+ * A group of status bar items that can be managed together
+ */
+export class StatusBarItemGroup implements vscode.Disposable {
+  private _items: StatusBarItem[] = [];
+  private _disposed: boolean = false;
 
+  /**
+   * Creates a new status bar item group with the given parameters
+   * @param items The parameters for the status bar items
+   * @returns A new StatusBarItemGroup instance
+   */
   static create(items: StatusBarItemParameters[]): StatusBarItemGroup {
     return new StatusBarItemGroup(items);
   }
 
+  /**
+   * Creates a new status bar item group
+   * @param items The parameters for the status bar items
+   */
   constructor(items: StatusBarItemParameters[]) {
-    const statusBarItems = items.map(StatusBarItem.create);
-    this.items = statusBarItems;
+    this._items = items.map(StatusBarItem.create);
   }
 
-  update(parameters: StatusBarItemParameters[]): void {
-    parameters.forEach((parameter) => {
-      const item = this.items.find((item) => item.id === parameter.id);
+  /**
+   * Gets the status bar items in this group
+   */
+  get items(): readonly StatusBarItem[] {
+    return this._items;
+  }
 
-      if (item) {
-        if (parameter.tooltip) {
-          item.tooltip = parameter.tooltip;
-        }
-
-        if (parameter.text) {
-          item.text = parameter.text;
-        }
-
-        if (parameter.color) {
-          item.color = parameter.color;
-        }
+  /**
+   * Adds a status bar item to this group
+   * @param item The status bar item to add
+   * @returns This status bar item group for chaining
+   */
+  add(item: StatusBarItem | StatusBarItemParameters): StatusBarItemGroup {
+    if (!this._disposed) {
+      const statusBarItem = item instanceof StatusBarItem 
+        ? item 
+        : StatusBarItem.create(item);
+        
+      // Avoid duplicates
+      if (!this._items.some(i => i.id === statusBarItem.id)) {
+        this._items.push(statusBarItem);
       }
-    });
+    }
+    return this;
   }
 
-  show(): void {
-    this.items.forEach((item) => item.show());
+  /**
+   * Removes a status bar item from this group
+   * @param id The ID of the status bar item to remove
+   * @returns This status bar item group for chaining
+   */
+  remove(id: string): StatusBarItemGroup {
+    if (!this._disposed) {
+      const index = this._items.findIndex(item => item.id === id);
+      if (index !== -1) {
+        this._items.splice(index, 1);
+      }
+    }
+    return this;
   }
 
-  hide(): void {
-    this.items.forEach((item) => item.hide());
+  /**
+   * Updates status bar items in this group
+   * @param parameters The parameters to update
+   * @returns This status bar item group for chaining
+   */
+  update(parameters: Partial<StatusBarItemParameters>[]): StatusBarItemGroup {
+    if (!this._disposed) {
+      parameters.forEach((parameter) => {
+        if (!parameter.id) {
+          return;
+        }
+        
+        const item = this._items.find((item) => item.id === parameter.id);
+        if (item) {
+          item.update(parameter);
+        }
+      });
+    }
+    return this;
   }
 
+  /**
+   * Shows all status bar items in this group
+   * @returns This status bar item group for chaining
+   */
+  show(): StatusBarItemGroup {
+    if (!this._disposed) {
+      this._items.forEach((item) => item.show());
+    }
+    return this;
+  }
+
+  /**
+   * Hides all status bar items in this group
+   * @returns This status bar item group for chaining
+   */
+  hide(): StatusBarItemGroup {
+    if (!this._disposed) {
+      this._items.forEach((item) => item.hide());
+    }
+    return this;
+  }
+
+  /**
+   * Disposes all status bar items in this group
+   */
   dispose(): void {
-    this.items.forEach((item) => item.dispose());
+    if (!this._disposed) {
+      this._items.forEach((item) => item.dispose());
+      this._items = [];
+      this._disposed = true;
+    }
   }
 
-  setInterval(callback: () => void, interval: number): void {
-    this.items.forEach((item) => item.setInterval(callback, interval));
+  /**
+   * Sets an interval for updating all status bar items in this group
+   * @param callback The function to call on the interval
+   * @param interval The interval in milliseconds
+   * @returns This status bar item group for chaining
+   */
+  setInterval(callback: () => void, interval: number): StatusBarItemGroup {
+    if (!this._disposed) {
+      this._items.forEach((item) => item.setInterval(callback, interval));
+    }
+    return this;
   }
 
-  clearInterval(): void {
-    this.items.forEach((item) => item.clearInterval());
+  /**
+   * Clears the update interval for all status bar items in this group
+   * @returns This status bar item group for chaining
+   */
+  clearInterval(): StatusBarItemGroup {
+    if (!this._disposed) {
+      this._items.forEach((item) => item.clearInterval());
+    }
+    return this;
   }
 }
