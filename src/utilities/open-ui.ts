8ae32b64638d89ff75d0cd59e56022e5dde4972b
Laura Whitaker <laura.whitaker@temporal.io>
2025-04-29 14:36:00 -0600

    Prevent double encoding

--- src/utilities/open-ui.ts
+++ src/utilities/open-ui.ts
@@ -2,21 +2,21 @@ import { env, Uri, window, ProgressLocation } from 'vscode';
 import { configuration } from './configuration';
 
 /**
- * Filter out undefined and null values from the query object.
+ * Build query string and filter out undefined and null values.
  */
-const filterQuery = (query: Record<string, string | undefined>) => {
+const buildQuery = (query: Record<string, string | undefined>) => {
   if (!query) {
-    return {};
+    return '';
   }
 
   return Object.entries(query)
     .filter(([_, value]) => value !== undefined && value !== null)
     .reduce(
       (acc, [key, value]) => {
-        acc[key] = String(value);
+        acc += `${acc ? '&' : ''}${key}=${value}`;
         return acc;
       },
-      {} as Record<string, string>,
+      '',
     );
 };
 
@@ -94,8 +94,7 @@ export async function openUI(
       },
       async () => {
         // Build the query string
-        const queryString = new URLSearchParams(filterQuery(query)).toString();
-
+        const queryString = buildQuery(query);
         // Build the URI
         const segments = ['namespaces', namespace];
         if (path) {
